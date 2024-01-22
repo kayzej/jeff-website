91b39ae439b14549d5f2843e36f135233f3a01c4
Jeff Kayzerman <Jeffrey Kayzerman>
2024-01-21 22:41:07 -0500

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col h-screen column items-center justify-center">
-      <h1 className="font-bold">Welcome to Jeff's Website!</h1>
+      <h1 className="font-bold">Welcome to Jeff{'\u0027'}s Website!</h1>
       <p>We are currently under maintenance and will be up and running soon!</p>
     </div>
   );
