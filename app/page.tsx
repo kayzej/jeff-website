21067aba7e734f281cf8df2e3bfb3c378672df9b
Jeffrey Kayzerman <jeffrey.kayzerman@gmail.com>
2024-02-03 21:02:36 -0500

    calling AWS a POS

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="flex flex-col h-screen column items-center justify-center">
       <h1 className="font-bold">Welcome to Jeff{'\u0027'}s Website!</h1>
       <p>We are currently under maintenance and will be up and running soon!</p>
+      <p>Also, AWS is a POS</p>
     </div>
   );
 }
