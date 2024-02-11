f2affd3648a85471380c235ab2da1dec5d73be1c
Jeffrey Kayzerman <jeffrey.kayzerman@gmail.com>
2024-02-10 22:14:18 -0500

    fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
 export default function Home() {
+  fetch("jeff-website-api-0.0.1-SNAPSHOT/helloworld")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+
   return (
     <div className="flex flex-col h-screen column items-center justify-center">
       <h1 className="font-bold">Welcome to Jeff{'\u0027'}s Website!</h1>
