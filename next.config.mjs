116bb9df5d657bab0c2f2136143967d66894d1cf
Jeffrey Kayzerman <jeffrey.kayzerman@gmail.com>
2024-02-10 23:47:00 -0500

    proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {};
+
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/api',
+                destination: 'http://82.165.214.184:8080:8000/' // Proxy to Backend
+            }
+        ]
+    }
+}
 
 export default nextConfig;
