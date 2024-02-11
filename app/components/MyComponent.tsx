93af1f5ad6c5bb67a9ec67dc157a073c383f3f8a
Jeffrey Kayzerman <jeffrey.kayzerman@gmail.com>
2024-02-10 23:08:53 -0500

    prettier

---
+++ app/components/MyComponent.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function MyComponent() {
+  let jsonResponse;
+
+  useEffect(() => {
+    fetch('jeff-website-api-0.0.1-SNAPSHOT/helloworld')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        jsonResponse = data;
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  });
+
+  return (
+    <div>
+      <p>Response: {jsonResponse} </p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-export default function Home() {
-  fetch('jeff-website-api-0.0.1-SNAPSHOT/helloworld')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
+import MyComponent from './components/MyComponent';
 
+export default function Home() {
   return (
     <div className="flex flex-col h-screen column items-center justify-center">
       <h1 className="font-bold">Welcome to Jeff{'\u0027'}s Website!</h1>
       <p>We are currently under maintenance and will be up and running soon!</p>
       <p>Also, AWS is a POS</p>
+      <MyComponent></MyComponent>
     </div>
   );
 }
