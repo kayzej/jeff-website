207f395971c6ca9e636aab8bf0387d22055237a3
Jeffrey Kayzerman <jeffrey.kayzerman@gmail.com>
2024-11-15 19:17:06 -0500

    background

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,24 +11,29 @@ const Navbar = () => {
     link.click();
   };
 
+  const buttons = [
+    { label: 'Home', href: '/' },
+    { label: 'About', href: '/about' },
+    { label: 'Resume', onClick: handleResumeDownload }, // Use onClick for the Resume button
+    { label: 'Test', href: '/test' }
+  ];
+
   return (
     <AppBar position="static" sx={{ backgroundColor: 'transparent', color: 'black', fontWeight: 'bold' }} elevation={0}>
       <Toolbar sx={{ color: 'black', fontWeight: 'bold' }}>
-        {/* <VerticalNavBar /> */}
         <Box>
-          <Button color="inherit" href="/">
-            Home
-          </Button>
-          <Button color="inherit" href="/about">
-            About
-          </Button>
-          <Button color="inherit" onClick={handleResumeDownload}>
-            Resume
-          </Button>
-          <Button color="inherit" href="/test">
-            Test
-          </Button>
-        </Box >
+          {buttons.map((button, index) => (
+            <Button
+              key={index}
+              color="inherit"
+              href={button.href}
+              onClick={button.onClick} // Only applies if the button has an onClick handler
+              sx={{ fontWeight: 'bold', fontSize: '1.2rem' }}
+            >
+              {button.label}
+            </Button>
+          ))}
+        </Box>
       </Toolbar >
     </AppBar >
   );
